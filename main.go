a04e33d98beb7691bcb422f54406c609152f950d
k-mat438 <a1sco00ocs1a@gmail.com>
2025-05-26 21:41:22 +0900

    Initial commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+// ginフレームワークをインポートします。
+import (
+	"gin-fleamarket/controllers"
+	"gin-fleamarket/models"
+	"gin-fleamarket/repositories"
+	"gin-fleamarket/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// ginのデフォルトのルーターを作成します。
+	// ルーターは、HTTPリクエストを処理するためのエンドポイントを定義します。
+	router := gin.Default()
+
+	// ルートエンドポイントを定義します。
+	// ここでは、"/ping"というパスにGETリクエストが来たときに、
+	// 無名関数を実行して、JSON形式でレスポンスを返します。
+	router.GET("/ping", func(c *gin.Context) {
+		// ここでcはjson形式のレスポンスを設定する
+		c.JSON(200, gin.H{
+			// gin.Hは、map[string]interface{}のエイリアスで、
+			// JSONレスポンスを簡単に作成するために使用されます。
+			"message": "pong",
+		})
+	})
+
+	items := []models.Item{
+		{ID: 1, Name: "Item1", Price: 1000, Description: "Description1", SoldOut: false},
+		{ID: 2, Name: "Item2", Price: 2000, Description: "Description2", SoldOut: true},
+		{ID: 3, Name: "Item3", Price: 3000, Description: "Description3", SoldOut: false},
+		{ID: 4, Name: "Item4", Price: 4000, Description: "Description4", SoldOut: true},
+	}
+
+	itemRepository := repositories.NewItemMemoryRepository(items)
+	itemService := services.NewItemService(itemRepository)
+	itemController := controllers.NewItemController(itemService)
+	router.GET("/items", itemController.FindAll)
+
+	router.Run("localhost:8080") // 0.0.0.0:8080 でサーバーを立てます。
+}
---
+++ repositories/item_repository.go
@@ -0,0 +1,21 @@
+// 商品一覧apiのリポジトリ(データアクセス層)
+
+package repositories
+
+import "gin-fleamarket/models"
+
+type IItemRepository interface {
+	FindAll() (*[]models.Item, error)
+}
+
+type ItemMemoryRepository struct {
+	items []models.Item
+}
+
+func NewItemMemoryRepository(items []models.Item) IItemRepository {
+	return &ItemMemoryRepository{items: items}
+}
+
+func (r *ItemMemoryRepository) FindAll() (*[]models.Item, error) {
+	return &r.items, nil
+}
